d2096daa61d2e4d6a8d23bc4905f5f4183e225fe
Prashant Sawat <258510@nttdata.com>
2022-11-25 23:24:28 +0530

    backend config changed

--- terraform/iwallet/providers.tf
+++ terraform/iwallet/providers.tf
@@ -7,12 +7,18 @@ terraform {
     }
   }
 
-  backend "azurerm" {}
+  backend "azurerm" {
+    subscription_id      = "475ecfec-e18a-4664-a8cd-6e3424489e4b"
+    resource_group_name  = "eus-hub-central-devops-rg"
+    storage_account_name = "eusdevopsstgacc01"
+    container_name       = "iwallet-sit"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
 
-    subscription_id = "475ecfec-e18a-4664-a8cd-6e3424489e4b"
+    # subscription_id = "475ecfec-e18a-4664-a8cd-6e3424489e4b"
 
 }
