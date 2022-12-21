a8924c123306c6b41d7d0026abea2527310f5cc7
praveenambati33 <106384062+praveenambati33@users.noreply.github.com>
2022-12-21 07:10:08 +0530

    provider changes

--- terraform/iwallet/providers.tf
+++ terraform/iwallet/providers.tf
@@ -1,20 +1,3 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "3.31.0"
-
-#     }
-#   }
-#   backend "azurerm" {}
-# }
-
-# provider "azurerm" {
-#   features {}
-# }
-
-
-
 terraform {
   required_providers {
     azurerm = {
@@ -23,16 +6,9 @@ terraform {
 
     }
   }
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
-  features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
-    }
-  }
-  subscription_id = "0c08f7e9-e572-4fca-b0ca-b9acf0eeda26"
-  tenant_id       = "9368fb9d-c4cb-4aec-843b-dda0af9beb59"
-  client_id       = "bc80739e-8a9c-447c-869c-fa7cf61532f7"
-  client_secret   = "Zj28Q~ck4aBCO7.VD.xQj5YjPGGpERW3TPmkZdwE"
+  features {}
 }
--- terraform/relaylistner/providers.tf
+++ terraform/relaylistner/providers.tf
@@ -1,22 +1,3 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "3.31.0"
-
-#     }
-#   }
-#   backend "azurerm" {}
-# }
-
-# provider "azurerm" {
-#   features {
-#     resource_group {
-#       prevent_deletion_if_contains_resources = false
-#     }
-#   }
-# }
-
 terraform {
   required_providers {
     azurerm = {
@@ -25,6 +6,7 @@ terraform {
 
     }
   }
+  backend "azurerm" {}
 }
 
 provider "azurerm" {
@@ -33,8 +15,4 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-  subscription_id = "0c08f7e9-e572-4fca-b0ca-b9acf0eeda26"
-  tenant_id       = "9368fb9d-c4cb-4aec-843b-dda0af9beb59"
-  client_id       = "bc80739e-8a9c-447c-869c-fa7cf61532f7"
-  client_secret   = "Zj28Q~ck4aBCO7.VD.xQj5YjPGGpERW3TPmkZdwE"
 }
