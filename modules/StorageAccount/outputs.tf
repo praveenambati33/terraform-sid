a8f2b8e80f15faca195dec17c29440f3d1569d6d
praveenambati33 <106384062+praveenambati33@users.noreply.github.com>
2022-12-21 09:00:41 +0530

    sa change

--- modules/StorageAccount/outputs.tf
+++ modules/StorageAccount/outputs.tf
@@ -1,6 +1,5 @@
 output "sa_primary_access_key_out"{
-    # value = { for storageaccount in azurerm_storage_account.storageaccount : storageaccount.primary_access_key =>  storageaccount.primary_access_key }
-    value = azurerm_storage_account.storageaccount.primary_access_key
+    value = { for storageaccount in azurerm_storage_account.storageaccount : storageaccount.name =>  storageaccount.primary_access_key }
     sensitive = true
 }
 
