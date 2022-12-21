600a5710147f3fa1bf76bd0203628e89575a4b9c
praveenambati33 <106384062+praveenambati33@users.noreply.github.com>
2022-12-21 08:16:25 +0530

    storage account module outputs change

--- modules/StorageAccount/outputs.tf
+++ modules/StorageAccount/outputs.tf
@@ -1,8 +1,8 @@
 output "sa_primary_access_key_out"{
-    value = azurerm_storage_account.storageaccount.primary_access_key
+    value = { for storageaccount in azurerm_storage_account.storageaccount : storageaccount.name =>  storageaccount.primary_access_key }
 }
 
 
 output "sa_name_out"{
-    value = azurerm_storage_account.storageaccount.name
+    value = { for storageaccount in azurerm_storage_account.storageaccount : storageaccount.name =>  storageaccount.name }
 }
