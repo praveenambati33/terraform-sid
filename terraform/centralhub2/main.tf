3e195098663667a3fa237deb06f8ea61eb170aeb
Prashant Sawat <258510@nttdata.com>
2022-12-29 21:40:35 +0530

    module source change for test

--- terraform/centralhub2/main.tf
+++ terraform/centralhub2/main.tf
@@ -4,7 +4,8 @@
 module "CHub_ResourceGroup" {
 
   # source            = "../../modules/ResourceGroup"
-  source            = "https://Org-SAMS@dev.azure.com/Org-SAMS/Proj_IaaC_P32H-Az-tenant-hHPHC-Subs/_git/modules/ResourceGroup"
+  # source            = "https://Org-SAMS@dev.azure.com/Org-SAMS/Proj_IaaC_P32H-Az-tenant-hHPHC-Subs/_git/modules/ResourceGroup"
+  source            = "./ResourceGroup"
   resourcegroupname = var.resourcegroupname
   location          = var.location
   tags              = var.tags
