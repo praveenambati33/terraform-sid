17a9843fc55adbd9b1e3e60d0d501390bac85097
Prashant Sawat <258510@nttdata.com>
2022-12-29 22:00:49 +0530

    module path changed in root module for test

--- terraform/centralhub2/main.tf
+++ terraform/centralhub2/main.tf
@@ -5,7 +5,8 @@ module "CHub_ResourceGroup" {
 
   # source            = "../../modules/ResourceGroup"
   # source            = "https://Org-SAMS@dev.azure.com/Org-SAMS/Proj_IaaC_P32H-Az-tenant-hHPHC-Subs/_git/modules/ResourceGroup"
-  source            = "./ResourceGroup"
+  # source            = "./ResourceGroup"
+  source            = "git::https://dev.azure.com/Org-SAMS/Proj_IaaC_P32H-Az-tenant-hHPHC-Subs/_git/modules"
   resourcegroupname = var.resourcegroupname
   location          = var.location
   tags              = var.tags
