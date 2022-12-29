6749bbc065eb5a3993f6f76f54772ce29600e244
Prashant Sawat <258510@nttdata.com>
2022-12-29 22:01:38 +0530

    provided module which is missed

--- terraform/centralhub2/main.tf
+++ terraform/centralhub2/main.tf
@@ -6,7 +6,7 @@ module "CHub_ResourceGroup" {
   # source            = "../../modules/ResourceGroup"
   # source            = "https://Org-SAMS@dev.azure.com/Org-SAMS/Proj_IaaC_P32H-Az-tenant-hHPHC-Subs/_git/modules/ResourceGroup"
   # source            = "./ResourceGroup"
-  source            = "git::https://dev.azure.com/Org-SAMS/Proj_IaaC_P32H-Az-tenant-hHPHC-Subs/_git/modules"
+  source            = "git::https://dev.azure.com/Org-SAMS/Proj_IaaC_P32H-Az-tenant-hHPHC-Subs/_git/modules/ResourceGroup"
   resourcegroupname = var.resourcegroupname
   location          = var.location
   tags              = var.tags
